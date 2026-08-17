67092911372a7088d47cdb37cfeaf079659c68e7
Jeonquan1988 <chengyunquan@gmail.com>
2026-08-17 11:46:14 +0200

    Add offline support asset: service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE='svt-offline-v1';
+const APP=['./v2.html','./supabase.min.js','./manifest.webmanifest','./icon.svg'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  const url=new URL(event.request.url);
+  if(url.origin!==self.location.origin)return;
+  if(event.request.mode==='navigate'){
+    event.respondWith(caches.match('./v2.html',{ignoreSearch:true}).then(cached=>{
+      const refresh=fetch(event.request).then(response=>{if(response.ok)caches.open(CACHE).then(cache=>cache.put('./v2.html',response.clone()));return response}).catch(()=>cached);
+      return cached||refresh;
+    }));
+    return;
+  }
+  event.respondWith(caches.match(event.request,{ignoreSearch:true}).then(cached=>cached||fetch(event.request).then(response=>{if(response.ok)caches.open(CACHE).then(cache=>cache.put(event.request,response.clone()));return response})));
+});
